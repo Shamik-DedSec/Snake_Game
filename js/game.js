271b2d41f7d39561556461cea3b780e564863743
Shamik <Shami@gmail.comk>
2026-09-24 10:43:34 +0530

    Initial commit

---
+++ js/game.js
@@ -0,0 +1,12 @@
+import { Snake } from './snake.js';
+
+export class Game {
+
+    constructor() {
+
+        this.rows = 20;
+        this.columns = 20;
+        this.snake = new Snake();
+    }
+
+}
